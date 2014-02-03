ea1159f6dac79fd7bb6ed8d73c8e03bee7cbb187
andrewjiang286 <andrewjiang2014@u.northwestern.edu>
2014-02-02 19:21:38 -0600

    implemented findnode

--- kademlia/rpc.go
+++ kademlia/rpc.go
@@ -4,6 +4,7 @@ package kademlia
 // other groups' code.
 
 import "net"
+import "sort"
 
 
 // Host identification.
@@ -74,18 +75,73 @@ type FindNodeResult struct {
     Err error
 }
 
+type ByDistance []*Contact
+
+func (a ByDistance) Swap(i,j int)       {a[i], a[j] = a[j], a[i]}
+func (a ByDistance) Len() int           {return len(a)}
+func (a ByDistance) Less(i, j int) bool {return a[i].NodeID.Less(a[j].NodeID)}
+
 func (k *Kademlia) FindNode(req FindNodeRequest, res *FindNodeResult) error {
     //first get everything in the bucket the node requested would have gone in and put it in a FoundNode slice
-    //Then get everything from the bucket above and below unless you have 20 nodes already
-        //keep putting everything in the slice
-        //need to be careful of edge cases: ends of list
-        //keep taking from adjacent buckets till you find 20 entries total
-        //need to put in check in case you search the whole list before finding 20
-    //Sort all the nodes you got by xor distance 
-        //use the go sort interface (planet example)
-    //return top 20, cut off the rest
+    bucket_num := node.NodeID.Xor(k.table.NodeID).PrefixLen()
+    bucket_slice := []*Contact
 
-    // remember to fill out result struct and call update
+    counter := 0
+    bucketcounter := 0
+
+    bucket := k.table.buckets[bucket_num]
+    for i := bucket.Front(); i != nil; i = i.Next() {
+        bucket_slice[counter] = i.Value.(*Contact)
+        counter++
+    }
+
+    //Then get everything from the bucket above and below unless you have 20 nodes already
+    for bucket_slice.Len() < 20 && flag == false{
+
+        //Get nodes from buckets to the left
+        if bucket_num - bucketcounter >= 0 {
+            bucket = k.table.buckets[bucket_num - bucketcounter]
+
+            for i := bucket.Front(); i != nil; i = i.Next() {
+                bucket_slice[counter] = i.Value.(*Contact)
+                counter++
+            }
+        }
+
+        //Get nodes from buckets to the right
+        if bucket_num + bucketcounter <= 159 {
+            bucket = k.table.buckets[bucket_num + bucketcounter]
+
+            for i := bucket.Front(); i != nil; i = i.Next() {
+                bucket_slice[counter] = i.Value.(*Contact)
+                counter++
+            }
+        }
+
+        //Increment bucket location counter
+        bucketcounter++
+
+        //Check for out of bounds
+        flag := bucket_num - bucketcounter < 0 && bucket_num + bucketcounter > 159
+    }
+
+    //Sort the slice by Xor distance to the input nodeID
+    sort.Sort(ByDistance(bucket_slice))
+
+    //Get 20 closest
+    bucket_slice = bucket_slice[0:19]
+    FoundNodes := []FoundNode
+
+    for i := 0 {
+        FoundNodes[i].IPAddr = bucket_slice[i].Host.String()
+        FoundNodes[i].Port = bucket_slice[i].Port
+        FoundNodes[i].NodeID = bucket_slice[i].NodeID
+    }
+
+    //Fill out result struct
+    res.Nodes = FoundNodes
+
+    //Update not called.  Important?  IDK
     return nil
 }
 
